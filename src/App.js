4aee9d856b1420f0f9b687b267bac3831e0aea83
Mouhammadou Oury Diallo <ryhow99@gmail.com>
2021-08-16 23:40:17 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import myWonderfulImage from "./imageInSrc.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:"solid 1px black" ,maxWidth:'100vw'}}>
+
+      <h1 className="title red">Mouhammadou Oury Diallo</h1>
+
+      <br></br>
+
+      <img src={myWonderfulImage} alt="img1" ></img>
+
+      <br></br> <br></br>
+
+      <img src="imageInPublic.png" alt="img2"></img>
+
+
+      <vidéo width="320" height="240" contrôles>
+
+      <source src="maVidéo.mp4" type="video/mp4" ></source>
+
+      </vidéo>
+
     </div>
   );
 }
